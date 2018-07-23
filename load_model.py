2c2c9d043dbc210b5816a8ceb04534f3b5fc1ba5
UTSAccount <zhenhua.zhang@student.uts.edu.au>
2018-07-23 13:32:48 +1000

    add coloring

--- load_model.py
+++ load_model.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 
 model_name = 'tiramisu_fc_dense67_model_12_func.json'
-weights_path = 'weights/prop_tiramisu_weights_67_12_func_10-e7_decay.best.hdf5'
+weights_path = 'weights/prop_tiramisu_weights_67_12_func_10-e7_decay150.hdf5'
 # load the model:
 def load_model():
 
@@ -26,6 +26,29 @@ def load_img():
 
     return input_data
 
+def color_map(result):
+
+    color_coding = [[255,0,0],[0,255,0],[0,0,255],
+                    [255,255,0],[255,0,255],[0,255,255],
+                    [125,0,0],[0,125,0],[0,0,125],
+                    [125,125,0],[125,0,125],[0,125,125]]
+
+    color_map = np.zeros((224,224,3), np.uint8)
+    # cv2.imshow("color_map", color_map)
+    # cv2.waitKey(1000000)
+
+    print(result.shape[0])
+    print(result.shape[1])
+    print(result[0])
+    # print(result[1,3])
+    for i in range(result.shape[0]):
+        for d in range(result.shape[1]):
+            color_map[i,d] = color_coding[result[i,d]]
+            print("[{},{}]".format(i,d))
+            print("result : ", result[i,d])
+            # print(result[i,d])
+        # exit()
+    return color_map
 
 def main():
     tiramisu = load_model()
@@ -41,6 +64,11 @@ def main():
     result = np.array(result)
     print(result)
 
+    map = color_map(result)
+
+    cv2.imshow("color_map", map)
+    cv2.resizeWindow('color_map', 600,600)
+    cv2.waitKey(1000000)
     #print(prediction)
     # print(prediction.shape)
     # w, h, c = np.indices(prediction.shape)
